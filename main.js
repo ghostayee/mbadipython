05e7d2ce3a5b51a99cda446b5dcba2e6c437e279
ghostayee <mayeeayee@outlook.com>
2026-03-31 21:51:42 +0300

    user

---
+++ main.js
@@ -0,0 +1,12 @@
+let dog = false;
+let cat = false;
+let fridge = true;
+
+if (dog) {
+  console.log("move towards the dog");
+}
+if (cat) {
+  console.log("moved towards cat");
+} else {
+  console.log("move towards the fridge");
+}
