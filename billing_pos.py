b45252fc27aab2f82fc24e5eb577348f021a2982
ghostayee <mayeeayee@outlook.com>
2026-03-31 20:45:29 +0300

    fast

--- billing_pos.py
+++ billing_pos.py
@@ -2,3 +2,11 @@ customer = "Moses"
 seller = "Gregory"
 item = "Subaru "
 item_amount = 300000
+customer_balance = 50000
+
+new_customer_balance = customer_balance - item_amount
+
+print(customer)
+print(item)
+print(item_amount)
+print(new_customer_balance)
---
+++ phony.py
@@ -0,0 +1,3 @@
+buyer = "jared"
+second_buyer = "kevin"
+list_serials = [3123, 3122, 4566, 7865]
