26f5f55a0d74714b3ab925e7314835b8536f7d40
ghostayee <mayeeayee@outlook.com>
2026-04-01 09:30:43 +0300

    def

--- phony.py
+++ phony.py
@@ -1,3 +1,10 @@
 buyer = "jared"
 second_buyer = "kevin"
 list_serials = [3123, 3122, 4566, 7865]
+
+def add_numbers(num1, num2):
+    results = num1 + num2
+    print("the results:")
+    
+add_numbers(30, 100)
+add_numbers(2,5)
